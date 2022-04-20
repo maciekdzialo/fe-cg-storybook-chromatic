7a137735f26844f2424a3dd4e56e9a4356905566
Maciek <68198@student-afm.edu.pl>
2022-04-20 18:43:06 +0200

    change

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { Button } from "../button/Button";
 
 export const App = () => {
   return (
     <div className="App">
       <div className="App__header">Hello</div>
-      <Button>
-        <span>Download</span>
-      </Button>
     </div>
   );
 };
